e13ca84a04e592124b44dd11c463bd2016901861
Yang-dotcom <72096892+Yang-dotcom@users.noreply.github.com>
2020-12-29 12:18:10 +0100

    ex6lab10 completed

--- lab10/ex6.py
+++ lab10/ex6.py
@@ -22,21 +22,84 @@
 
 FILE_NAME = 'playFairCypherTest.txt'
 KEYWORD = input("Enter key word in the cypher: ")
+ALPHABET = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
 
 def del_dupl(word):
-    no_dupl = "".join(dict.fromkeys(word))
+    no_dupl = "".join(dict.fromkeys(word)).upper()
+    if "J" in no_dupl:
+        no_dupl = no_dupl.replace("J", "I")
     return no_dupl
 
 def main():
-    tbl = tbl5X5()
-    print(del_dupl(KEYWORD), tbl)
+    """MAIN ENTRY POINT"""
+    tbl = tbl5X5(KEYWORD)
+    print(del_dupl(KEYWORD))
+    display_matrix(tbl)
 
-def tbl5X5():
-    tbl = [[" " for _ in range(5)] for _ in range(5)]
+    try:
+        with open('playFairCypherTest.txt') as cypher_test:
+            for line in cypher_test:
+                try:
+                    x = []
+                    x = plain_text(line)
+                    print(f"Original: {x}")
+                    print(cypher(x, tbl))
+                except ValueError as error:
+                    print(f"Unexpected value entered: {error}")
+
+    except OSError as problem:
+        print(f"Error encountered: {problem}")
+
+def tbl5X5(word):
+    full_string = ''.join(list(del_dupl(word)) + [c for c in ALPHABET if c not in del_dupl(word)])
+    tbl = [["" for _ in range(5)] for _ in range(5)]
+    print(f"{full_string}")
+    for i in range(5):
+        for j in range(5):
+            tbl[i][j] = full_string[i * 5 + j]
     return tbl
 
-def cypher():
-    pass
+def cypher(plain_text, tbl):
+    # check row
+    list_temp = []
+    for i in range(len(plain_text)):
+        first_chr = plain_text[i][0]
+        second_chr = plain_text[i][1]
+        index_first = [(i, el.index(first_chr)) for i, el in enumerate(tbl) if first_chr in el]
+        index_second =[(i, el.index(second_chr)) for i, el in enumerate(tbl) if second_chr in el]
+        list_temp.append(index_first)
+        list_temp.append(index_second)
+        # print(first_chr, second_chr)
+        # print(type(index_first[0][0]), type(index_second[0][1]))
+        # print(tbl[index_first[0][0], index_second[0][1]])
+        if index_first[0][0] == index_second[0][0] or index_first[0][1] == index_second[0][1]:
+            # print(f"OK for {first_chr} and {second_chr}")
+            plain_text[i][0], plain_text[i][1] = plain_text[i][1], plain_text[i][0]
+            # print(f"Switched 1: {plain_text[i][0]}, {plain_text[i][1]}")
+        else:
+            plain_text[i][0] = tbl[index_first[0][0]][index_second[0][1]]
+            plain_text[i][1] = tbl[index_second[0][0]][index_first[0][1]]
+            # print(f"Switched 2: {plain_text[i][0]}, {plain_text[i][1]}")
+
+    return plain_text
+
+
+
+def plain_text(user_line):
+    user_line = ''.join(user_line.split()).replace(".", "").upper()
+    if len(user_line) % 2 != 0:
+        user_line += "Z"
+    list_line = [["" for x in range(2)] for c in range(len(user_line) // 2)]
+    for i in range(len(user_line) // 2):
+        for j in range(2):
+            list_line[i][j] = user_line[2 * i + j]
+    return list_line
+
+
+
+def display_matrix(tbl):
+    for x in tbl:
+        print(x)
 
 if __name__ == '__main__':
     main()
